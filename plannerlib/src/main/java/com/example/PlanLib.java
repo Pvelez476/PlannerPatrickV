19fe9f592a3426a985de23743be0acf64d6b6be7
Pvelez476 <pvelez@students.mchenry.edu>
2015-11-02 23:01:09 -0600

    Initial commit

---
+++ plannerlib/src/main/java/com/example/PlanLib.java
@@ -0,0 +1,47 @@
+package com.example;
+
+public class PlanLib {
+    public static void main(String[] args) {
+
+        int sunday = 1;
+        int monday = 2;
+        int tuesday = 3;
+        int wednesday = 4;
+        int thursday = 5;
+        int friday = 6;
+        int saturday = 7;
+
+
+        if (sunday >=1) {
+            System.out.println("Sunday");
+        }
+
+        if (monday >=2) {
+            System.out.println("Monday");
+        }
+
+        if (tuesday >=3) {
+            System.out.println("Tuesday");
+        }
+        
+        if (wednesday >=4) {
+            System.out.println("Wednesday");
+        }
+
+        if (thursday >=5) {
+            System.out.println("Thursday");
+        }
+
+        if (friday >=6) {
+            System.out.println("Friday");
+        }
+
+        if (saturday >=7) {
+            System.out.println("Saturday");
+        }
+
+
+
+    }
+
+}
