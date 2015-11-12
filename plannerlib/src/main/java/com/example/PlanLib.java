7f7b97fb365993676f53a7173be3f8147e3c4077
Pvelez476 <pvelez@students.mchenry.edu>
2015-11-12 02:53:08 -0600

    VERSION4

--- plannerlib/src/main/java/com/example/PlanLib.java
+++ plannerlib/src/main/java/com/example/PlanLib.java
@@ -1,8 +1,26 @@
 package com.example;
 
+import java.util.*;
+
+
 public class PlanLib {
+
+    //MAIN METHOD.
     public static void main(String[] args) {
 
+
+        //Code to call Calendar.
+        Calendar cal = Calendar.getInstance();
+
+
+        System.out.println("Calendar's Year: " + cal.get(Calendar.YEAR));
+        System.out.println("Calendar's Month: " + cal.get(Calendar.MONTH));
+        System.out.println("Calendar's Day: " + cal.get(Calendar.DATE));
+        //////////////////////////////////////////////////////////////////
+
+
+
+        //Ints to represent the days.
         int sunday = 1;
         int monday = 2;
         int tuesday = 3;
@@ -11,37 +29,37 @@ public class PlanLib {
         int friday = 6;
         int saturday = 7;
 
-
-        if (sunday >=1) {
+        //If Statements to call days.
+        if (sunday >= 1) {
             System.out.println("Sunday");
         }
 
-        if (monday >=2) {
+        if (monday >= 2) {
             System.out.println("Monday");
         }
 
-        if (tuesday >=3) {
+        if (tuesday >= 3) {
             System.out.println("Tuesday");
         }
-        
-        if (wednesday >=4) {
+
+        if (wednesday >= 4) {
             System.out.println("Wednesday");
         }
 
-        if (thursday >=5) {
+        if (thursday >= 5) {
             System.out.println("Thursday");
         }
 
-        if (friday >=6) {
+        if (friday >= 6) {
             System.out.println("Friday");
         }
 
-        if (saturday >=7) {
+        if (saturday >= 7) {
             System.out.println("Saturday");
         }
-
 
 
     }
 
 }
+
