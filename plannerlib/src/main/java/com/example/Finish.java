c849b777c56b677b66fbc8cb146e1efabf706721
Pvelez476 <pvelez@students.mchenry.edu>
2015-12-03 08:57:17 -0600

    Version7

---
+++ plannerlib/src/main/java/com/example/Finish.java
@@ -0,0 +1,22 @@
+package com.example;
+
+/**
+ * Created by Patrick on 12/3/2015.
+ */
+
+class Finish{
+String fin;
+}
+
+
+
+class End {
+    public static void main (String[] args){
+
+
+        Finish finish1 = new Finish();
+
+
+    }
+}
+
