642c79adfa691f7e2e356828dbb84154dc508b39
Pvelez476 <pvelez@students.mchenry.edu>
2015-12-03 09:25:55 -0600

    Version7

--- plannerlib/src/main/java/com/example/PlanLib.java
+++ plannerlib/src/main/java/com/example/PlanLib.java
@@ -10,43 +10,67 @@ public class PlanLib {
     //MAIN METHOD.
     public static void main(String[] args) {
 
-
+        Calendar cal = Calendar.getInstance();
 
         int start = 0;
-        System.out.println("Enter the Day:");
-        Scanner inputFromUser = new Scanner (System.in);
-        int finish;
-        if (inputFromUser.hasNextLine()){
 
-             finish = inputFromUser.nextInt();
-            System.out.println("Enter the time");
-        }else{
-             finish =1;
-            System.out.println("Error");
-        }
+        System.out.println("Hello! Welcome to the Planner.");
+        System.out.println("");
 
-        Scanner inputWords = new Scanner(System.in);
-        String words = inputWords.nextLine();
-        while (start < finish) {
-            System.out.println(words);
-            start +=1;
-        }
+        System.out.println("Today's Date is:");
+
 
         //Code to call Calendar.
-       // Calendar cal = Calendar.getInstance();
+        System.out.println(cal.get(Calendar.DATE)+"/" + cal.get(Calendar.MONTH)+"/" + cal.get(Calendar.YEAR));
 
 
-       // System.out.println("Calendar's Year: " + cal.get(Calendar.YEAR));
-       // System.out.println("Calendar's Month: " + cal.get(Calendar.MONTH));
-       // System.out.println("Calendar's Day: " + cal.get(Calendar.DATE));
 
-       // Code to call Time.
-       // System.out.println("The Hour is: " + cal.get(Calendar.HOUR_OF_DAY));
-       // System.out.println("The Minute is: " + cal.get(Calendar.MINUTE));
-       // System.out.println("The Second is: " + cal.get(Calendar.SECOND));
-        //////////////////////////////////////////////////////////////////
+        // Code to call Time.
+        System.out.println("");
+        System.out.println("Today's Time is:");
+        System.out.println(cal.get(Calendar.HOUR_OF_DAY) + ":" + cal.get(Calendar.MINUTE) + cal.get(Calendar.AM_PM));
 
 
+
+        System.out.println("");
+        System.out.println("Enter a Date:");
+
+        Scanner inputWords = new Scanner(System.in);
+
+
+        if (inputWords.hasNextLine()) {
+            System.out.println("Enter the time");
+            Scanner inputTime = new Scanner(System.in);
+
+
+            if (inputTime.hasNextLine()) {
+                System.out.println("Enter Your Task");
+                Scanner inputTask = new Scanner(System.in);
+
+
+                String words = inputWords.nextLine();
+                String time = inputTime.nextLine();
+                String task = inputTask.nextLine();
+
+
+
+                int finish = 1;
+                {
+                    while (start < finish) {
+                        System.out.println("");
+
+
+                        System.out.println("You will complete:  "  +  task  +   " On: " +  time  + " At " +  time  +  " ......Or else! ");
+                        start = 1;
+                    }
+
+
+
+
+
+                }
+
+            }
+        }
     }
-
 }
